84813f0d3c8a896d2cf11f00692bd8f04103b73e
angelinawwu <angelinawu05@gmail.com>
2025-09-26 10:49:09 -0700

    scuffed background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import QRCode from 'qrcode';
+
+import GradientBlinds from './components/GradientBlinds';
+
 
 interface ScrapingResult {
   year?: string;
@@ -602,6 +605,22 @@ function App() {
 
   return (
     <div className="App">
+      <div className="background-container">
+        <GradientBlinds
+          gradientColors={['#FF9FFC', '#5227FF']}
+          angle={0}
+          noise={0.3}
+          blindCount={12}
+          blindMinWidth={50}
+          spotlightRadius={0.5}
+          spotlightSoftness={1}
+          spotlightOpacity={1}
+          mouseDampening={0.15}
+          distortAmount={0}
+          shineDirection="left"
+          mixBlendMode="lighten"
+        />
+      </div>
       <header className="App-header">
         {currentPage === 'welcome' && renderWelcomePage()}
         
