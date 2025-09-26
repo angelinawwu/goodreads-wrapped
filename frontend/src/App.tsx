9dce4fe6485a3f78ea5cf6757ecd95ab51a9eb29
angelinawwu <angelinawu05@gmail.com>
2025-09-26 10:53:17 -0700

    fixing mouse tracking for bg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -608,14 +608,14 @@ function App() {
       <div className="background-container">
         <GradientBlinds
           gradientColors={['#FF9FFC', '#5227FF']}
-          angle={0}
+          angle={20}
           noise={0.3}
           blindCount={12}
           blindMinWidth={50}
           spotlightRadius={0.5}
           spotlightSoftness={1}
           spotlightOpacity={1}
-          mouseDampening={0.15}
+          mouseDampening={0.05}
           distortAmount={0}
           shineDirection="left"
           mixBlendMode="lighten"
